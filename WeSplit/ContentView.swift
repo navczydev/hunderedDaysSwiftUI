7677adfbe0424280c5c079dcc36c938399aa5b72
Navjot Singh <Nav_Singh@manulife.com>
2020-02-17 19:09:31 -0500

    chapter1 done

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,23 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    //for state mgt.
+    @State private var tabCount = 0
+    @State private var name = ""
+    let students = ["Harry","Nav","Gagan"]
+    @State var selectedStudent =  "Harry"
     var body: some View {
-        NavigationView{
-            Form{
-                Section{
-                    Text("Hello, World!")
-                    Text("Hello, World!")
-                }
-                Section{
-                    Text("Hello, World!")
-                    Text("Hello, World!")
-                }
-
-            }
+        Text("Hello world!!")
+        //NavigationView{
+            //program state
+//            Button("TabCount is \(tabCount)"){
+//                self.tabCount += 1
+//            }
+//            Form{
+//                Section{
+//                    Text("Hello, World!")
+//                    Text("Hello, World!")
+//                }
+//                Section{
+//                    Text("Hello, World!")
+//                    Text("Hello, World!")
+//                }
+//
+//            }
              //displayMode requires with Text but not with raw string
 //            .navigationBarTitle(Text("SwiftUI"),displayMode: .inline)
-            .navigationBarTitle("SwiftUI")
-        }
+            //.navigationBarTitle("SwiftUI")
+       // }
+        
+        /**
+                $ makes 2 way binding
+         */
+//        Form{
+//            TextField("Enter your name",text: $name)
+//            Text("Name is \(name)")
+//        }
+        
+        //for loop to create view
+      //  Form{
+//            ForEach(0 ..< 100){
+//                Text("Row \($0)")
+//            }
+//            Picker("Select your student",selection: $selectedStudent){
+//                ForEach(0 ..< students.count){
+//                    Text(self.students[$0])
+//                }
+//            }
+        //}
         
     }
         
